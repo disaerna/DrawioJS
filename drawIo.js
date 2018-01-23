743e031b5c1d4da10874e9557b81efb8e65737d9
Unnsteinn Garðarsson <unnsteinngardarsson@Unnsteinns-MacBook-Pro.local>
2018-01-23 19:33:48 +0000

    some minor work on the form objects, jquery also connected

--- drawIo.js
+++ drawIo.js
@@ -1,3 +1,16 @@
+$(document).ready(function(){
+    $("#circle").on("click", function(){
+        console.log("Circle click works")
+        // load circle somehow
+    });
+
+    $("#myCanvas").on("mousedown", function(){
+        // load current element(form, text, etc) and apply to canvas
+        console.log("draw element");
+    });
+
+});
+
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 ctx.moveTo(0,0);
--- forms.js
+++ forms.js
@@ -1,28 +1,55 @@
+// FORM
 
+// Constructor
 function Form(type){
     this.type = type;
 }
 
+// Prototypes
 Form.prototype.getType = function(){
     console.log(this.type);
 }
 
+// RECTANGLE
+
+// Prototype hieararchy declaration
 Rectangle.prototype = Object.create(Form.prototype);
 Rectangle.prototype.constructor = Rectangle;
 
+// Constructor
 function Rectangle(height, width){
     Form.call(this, 'rectancle')
     this.height = height;
     this.width = width;
+    this.area = this.width * this.height;
 }
 
-Rectangle.prototype.getArea = function(){
-    console.log(this.height * this.width);
+// Prototypes
+Rectangle.prototype.getNumbers = function(){
+    return {
+        height: this.height, 
+        width: this.width,
+        area: this.height * this.width,
+    };
 }
+// CIRCLE
 
+Circle.prototype = Object.create(Form.prototype);
+Circle.prototype.constructor = Circle;
 
-function Circle(){
+// Constructor
+
+function Circle(diameter){
     Form.call(this, 'circle')
+    this.diameter = diameter;
+    this.radius = this.diameter / 2;
+    this.circumference = 2 * Math.PI * this.radius;
+}
+
+// Prototypes
+
+Circle.prototype.getCircumference = function(){
+    return this.circumference;
 }
 
 function Line(){
@@ -34,5 +61,7 @@ function Text(){
 }
 
 var rect = new Rectangle(400, 400);
-rect.getArea();
-rect.getType();
+var circle = new Circle(1000);
+var rectNumbers = rect.getNumbers();
+console.log(rectNumbers);
+console.log(circle.getCircumference());
